d0e2b930dd34dc43a894193cb93ed844484afccb
Shiva Kanyal <kanyalshiva1@gmail.com>
2021-03-25 16:29:33 +0530

    signup user functionality is completed.

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const { validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
 
 const User = require("../models/user");
 
@@ -10,4 +11,21 @@ exports.signup = (req, res, next) => {
   const email = req.body.email;
   const name = req.body.name;
   const password = req.body.password;
+
+  bcrypt
+    .hash(password, 12)
+    .then((hashedpassword) => {
+      const user = new User({
+        name: name,
+        email: email,
+        password: hashedpassword,
+      });
+      return user.save();
+    })
+    .then((result) => {
+      res.status(201).json({ message: "user Created", id: result._id });
+    })
+    .catch((err) => {
+      res.status(500).send({ message: err.message });
+    });
 };
