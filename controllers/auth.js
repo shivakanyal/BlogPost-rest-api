e5c1a93b674dbaaca3e33d3875ae53e69c13d37a
Shiva Kanyal <kanyalshiva1@gmail.com>
2021-03-26 12:08:30 +0530

    Login functionality is added using jwt

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,6 @@
 const { validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
-
+const jwt = require("jsonwebtoken");
 const User = require("../models/user");
 
 exports.signup = (req, res, next) => {
@@ -24,6 +24,41 @@ exports.signup = (req, res, next) => {
     })
     .then((result) => {
       res.status(201).json({ message: "user Created", id: result._id });
+    })
+    .catch((err) => {
+      res.status(500).send({ message: err.message });
+    });
+};
+
+exports.login = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  let loadedUser;
+  User.findOne({ email: email })
+    .then((user) => {
+      if (!user) {
+        return res
+          .status(401)
+          .send({ message: "User with given email is not found." });
+      }
+      loadedUser = user;
+      console.log("password", password);
+      console.log("user.password", user.password);
+      return bcrypt.compare(password, user.password);
+    })
+    .then((isEqual) => {
+      console.log("isEqual", isEqual);
+      if (!isEqual) {
+        return res
+          .status(401)
+          .send({ message: "email or password is not found." });
+      }
+      const token = jwt.sign(
+        { email: loadedUser.email, id: loadedUser._id.toString() },
+        "longSecretKey",
+        { expiresIn: "1h" }
+      );
+      res.status(200).send({ token, userId: loadedUser._id });
     })
     .catch((err) => {
       res.status(500).send({ message: err.message });
